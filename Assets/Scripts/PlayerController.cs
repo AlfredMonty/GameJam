177fae319e2ca609627ecd020f75975e9ea826cb
Boaz Quinones <59629660+AlfredMonty@users.noreply.github.com>
2022-06-10 13:45:32 -0400

    First Commit
    Testing

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem; 
+
+public class PlayerController : MonoBehaviour
+{
+    public Rigidbody2D rBody; 
+    [SerializeField] public float mSpeed = 0.75f; 
+    public InputAction pMovement;
+    Vector2 mDirection = Vector2.zero; 
+    // Start is called before the first frame update
+    void Start() {
+        
+    }
+
+    private void OnEnable() {
+        pMovement.Enable();
+    }
+    private void OnDisable() {
+        pMovement.Disable(); 
+    }
+    // Update is called once per frame
+    void Update() {
+        mDirection = pMovement.ReadValue<Vector2>();
+    }
+    
+    private void FixedUpdate() {
+        rBody.velocity = new Vector2(mDirection.x * mSpeed, mDirection.y * mSpeed);
+    }
+}
